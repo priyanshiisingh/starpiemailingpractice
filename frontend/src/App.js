3ce87c1dad800b273eaa4317ccfaca9afb490e85
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-12-22 17:01:08 +0530

    done, working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [newEmail, setNewEmail] = useState("");
+  function addemail(e) {
+    e.preventDefault();
+    let item = newEmail;
+    let body = {
+      data: {
+        emailId: item,
+      },
+    };
+
+    fetch(`${process.env.REACT_APP_BACKEND}api/user-emails`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then(() => {
+      setNewEmail("");
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <form className="form" onSubmit={addemail}>
+          <input
+            type="email"
+            className="email_input"
+            placeholder="Enter email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.currentTarget.value)}
+          />
+          <button type="submit" className="email_button">
+            Add email
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
-
 export default App;
